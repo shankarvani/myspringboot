dd662594d5fd5797bdb09f23b716379f17f002e5
varshika <vani212008@gmail.com>
2022-06-01 12:23:16 -0400

    first commit

---
+++ src/main/java/com/capproject/wardeploy/WardeployApplication.java
@@ -0,0 +1,13 @@
+package com.capproject.wardeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WardeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WardeployApplication.class, args);
+	}
+
+}
